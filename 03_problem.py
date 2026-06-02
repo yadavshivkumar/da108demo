a0e134095bf329e5ef4dc45d5047217b883614af
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-06-02 15:40:10 +0530

    Add files via upload

--- 03_problem.py
+++ 03_problem.py
@@ -1,4 +1,7 @@
-name = "Harry is a good   boy and "
+a = (23 ,343, "Shiv")
 
-print(name.find("   "))
+a[2] = "Shiva" # This will raise a TypeError because tuples are immutable.
+# To modify the tuple, you need to create a new one.    
+a = (23, 343, "Shiva")  # Create a new tuple with the desired change
 
+print(a)  # Output: (23, 343, 'Shiva')      
