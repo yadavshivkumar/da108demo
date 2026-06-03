8427b85e9d9e65fcd25ccb1fa6f36fdc45a10206
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-06-03 12:47:48 +0530

    Add files via upload

--- 05_problem.py
+++ 05_problem.py
@@ -1,3 +1,4 @@
-letter = "Dear Harry, \n\tthis python course is nice. \nThanks!"
+a = (23, 3 , 3, 3, 3, 3, 3,  3 , 3, 43, 54)
 
-print(letter)
+n =  a.count(3)  # This will count the number of occurrences of 0 in the tuple
+print(n)  
